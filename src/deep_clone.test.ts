5ccbb1f2844ee1a26830668d2f9873303c51bfb0
Halvard Mørstad <halvard@simplyundoable.com>
2021-06-14 00:36:56 +0200

    deep clone WIP

---
+++ src/deep_clone.test.ts
@@ -0,0 +1,90 @@
+import { deepClone } from "./deep_clone.ts";
+import {
+  assertEquals,
+  assertNotEquals,
+  assertNotStrictEquals,
+  assertStrictEquals,
+} from "https://deno.land/std@0.98.0/testing/asserts.ts";
+
+Deno.test({
+  name: "Test deepClone - null",
+  fn(): void {
+    const expected = null;
+
+    const initial = null;
+    const cloned = deepClone(initial);
+
+    assertEquals(cloned, expected);
+    assertStrictEquals(cloned, initial);
+  },
+});
+
+Deno.test({
+  name: "Test deepClone - string",
+  fn(): void {
+    const expected = "abc";
+
+    const initial = "abc";
+    const cloned = deepClone(initial);
+
+    assertEquals(cloned, expected);
+    assertStrictEquals(cloned, initial);
+  },
+});
+
+Deno.test({
+  name: "Test deepClone - array",
+  fn(): void {
+    const expected = [1, 2, 3];
+
+    const initial = [1, 2, 3];
+    const cloned = deepClone(initial);
+
+    assertEquals(cloned, expected);
+    assertNotStrictEquals(cloned, initial);
+
+    cloned[1] = 4;
+
+    assertEquals(initial, expected);
+    assertEquals(cloned, [1, 4, 3]);
+    assertNotEquals(initial, cloned);
+  },
+});
+
+Deno.test({
+  name: "Test deepClone - object",
+  fn(): void {
+    const expected = { a: 1, b: 2, c: 3 };
+
+    const initial = { a: 1, b: 2, c: 3 };
+    const cloned = deepClone(initial);
+
+    assertEquals(cloned, expected);
+    assertNotStrictEquals(cloned, initial);
+
+    cloned.b = 4;
+
+    assertEquals(initial, expected);
+    assertEquals(cloned, { a: 1, b: 4, c: 3 });
+    assertNotEquals(initial, cloned);
+  },
+});
+
+Deno.test({
+  name: "Test deepClone - nested",
+  fn(): void {
+    const expected = { a: 1, b: [{ d: 4, e: { 5: "f" } }], c: 3 };
+
+    const initial = { a: 1, b: [{ d: 4, e: { 5: "f" } }], c: 3 };
+    const cloned: any = deepClone(initial);
+
+    assertEquals(cloned, expected);
+    assertNotStrictEquals(cloned, initial);
+
+    cloned["b"] = 4;
+
+    assertEquals(initial, expected);
+    assertEquals(cloned, { a: 1, b: 4, c: 3 });
+    assertNotEquals(initial, cloned);
+  },
+});
---
+++ src/deep_clone.ts
@@ -0,0 +1,94 @@
+// deno-lint-ignore-file no-explicit-any
+import { type } from "./type.ts";
+
+export type PredicateOptions<T = any> = {
+  object: T;
+  predicates: Predicate[];
+  cloner: Cloner<T>;
+};
+/** A predicate takes an element, and returns an array
+ * with a boolean for if the cloning was executed and the
+ * return value contains a clone, the second element is the
+ * cloned element if previous was true.
+ *
+ * Returns [wasCloned, clonedElement]
+ */
+export type Predicate<T = any> = (options: PredicateOptions<T>) => [boolean, T];
+export type Cloner<T = any> = (options: PredicateOptions<T>) => T;
+export type DeepCloneOptions = {
+  predicates?: Predicate[];
+  override?: boolean;
+};
+
+export const cloneArray: Predicate<any[]> = ({
+  object,
+  predicates,
+  cloner,
+}) => {
+  if (type(object) !== "array") return [false, []];
+
+  const newObject: any[] = [];
+
+  for (const el of object) {
+    newObject.push(cloner({ object: el, predicates, cloner }));
+  }
+
+  return [true, newObject];
+};
+
+export const cloneObject: Predicate = ({
+  object,
+  predicates,
+  cloner,
+}) => {
+  if (type(object) !== "object") return [false];
+
+  const newObject: any = {};
+
+  for (const [key, value] of Object.entries(object)) {
+    newObject[key] = cloner({ object: value, predicates, cloner });
+  }
+
+  return newObject;
+};
+
+export const cloneDate: Predicate<Date> = ({ object }) => {
+  if (type(object) !== "date") return [false];
+
+  const newObject: any = new Date();
+  newObject.setTime(object.getTime());
+  return newObject;
+};
+
+const cloner: Cloner = (options) => {
+  for (const fn of options.predicates) {
+    console.log(fn)
+    const [wasCloned, clonedElement] = fn(options);
+
+    if (wasCloned) return clonedElement;
+  }
+
+  return options.object;
+};
+
+/** Will deep clone any element using provided predicates.
+ * By default, deep clone is only supported for primitives, json objects and arrays.
+ * If you need a custom object cloner, this can be provided in the options.
+ * See cloneArray, cloneObject and cloneDate for predicate samples
+ *
+ *        const obj = {a:[{b:1}]}
+ *        const clone = deepClone(obj)
+ *        clone.a = 2
+ *        console.log(obj) // {a:[{b:1}]}
+ *        console.log(clone) // {a:2}
+ */
+export function deepClone<T>(object: T, options: DeepCloneOptions = {}): T {
+  const predicates: Predicate[] = [...(options.predicates ?? [])];
+
+  if (!options.override) {
+    predicates.push(cloneArray, cloneObject);
+  }
+
+  return cloner({ object, predicates, cloner });
+}
+
