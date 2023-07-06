e298f49af92153abe2c22dd92799b573814fdc56
Halvard Mørstad <halvard@simplyundoable.com>
2023-07-06 16:30:23 +0200

    fixed tests

--- lib/default/fetcher.test.ts
+++ lib/default/fetcher.test.ts
@@ -4,10 +4,14 @@ import { assertEquals, assertThrows } from "./deps.ts";
 Deno.test({
   name: "Test FetcherError",
   fn() {
-    const error = new FetcherError(
-      new Response(undefined, { statusText: "test" }),
-    );
-    assertEquals(error.message, "test");
+    const error = new FetcherError({
+      bodyString: "test",
+      headers: new Headers(),
+      status: 200,
+      statusText: "OK",
+      url: "https://example.com",
+    });
+    assertEquals(error.message, "OK");
     assertEquals(error.name, "FetcherError");
 
     assertThrows(
@@ -15,7 +19,7 @@ Deno.test({
         throw error;
       },
       FetcherError,
-      "test",
+      "OK",
     );
   },
 });
